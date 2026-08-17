7e766752b6747bdf75df35c2d5e66c20b61c7a92
deadworm <zhangxu.nk@hotmail.com>
2026-08-17 10:17:22 +0200

    up

--- VMC_MPS/VMCmain.jl
+++ VMC_MPS/VMCmain.jl
@@ -1,117 +1,37 @@
 #dmrg test for Heisenberg model
-using TensorKit
+using TensorKit, MPSKit
 using KrylovKit
 using Random
 using Combinatorics
 
 include("mpstools.jl")
-include("pepstools.jl")
 
-lx = 6
-ly = 6
-l = lx * ly
-
-phySpace = Rep[U₁]((1 / 2) => 1, (-1 / 2) => 1)
-config_sz = []
-for ix = 1:lx
-    for iy = 1:ly
-        if (ix + iy) % 2 == 0
-            push!(config_sz, Rep[U₁]((1 / 2) => 1))
-        else
-            push!(config_sz, Rep[U₁]((-1 / 2) => 1))
-        end
-    end
-end
-
+l = 4
+n = 2
 bond_dim = 4
-bond_cut = 10
-dt = 0.01
-nsweep = 40
+phySpace = Vect[FermionParity⊠U1Irrep]((0, 0) => 1, (1, 1) => 1)
 
-#generate qn randomly for PEPS
-peps = PEPSinit(lx, ly, config_sz, phySpace)
-rdPEPS!(lx, ly, peps, bond_dim, dt, nsweep)
+m = initial_mps(l, n, phySpace, bond_dim);
 
-#pick physical indices of PEPS
-# config_sz[1]=config_sz[2]
-# config_sz[2]=config_sz[3]
-fpeps = Vector{TensorMap}(undef, l)
-for ix = 1:lx
-    for iy = 1:ly
-        i = (ix - 1) * ly + iy
-        ft = TensorMap(ones, config_sz[i] ← phySpace)
-        @tensor fpeps[i][p l d; u r] := peps[i][p0 l d; u r] * ft[p; p0]
-        # @show fpeps[i].data==peps[i].data
-    end
+configtest=[0 0 1 1;
+0 1 0 1;
+0 1 1 0;
+1 0 0 1;
+1 0 1 0;
+1 1 0 0];
+normtest=0;
+cm = zeros(1, 6);
+for id = 1:6
+    config = configtest[id, :]
+    cm[id] = get_config(m, config)
+    println("config: ", config, " coefficient: ", cm[id])
+    normtest += abs(cm[id])^2
 end
+println("normtest: ", normtest)
 
-#test PEPS contraction
-# @time "PEPS contraction test" begin
-mps_d = Vector{TensorMap}(undef, l)
-mps_u = Vector{TensorMap}(undef, l)
-for ix = 1:lx
-    mps_d[ix] = fpeps[(ix-1)*ly+1]
-    mps_u[(ly-1)*lx+ix] = permute(fpeps[(ix-1)*ly+ly], (1, 2, 4), (3, 5))
-end
-for iy = 2:ly
-    peps_d = Vector{TensorMap}(undef, lx)
-    peps_u = Vector{TensorMap}(undef, lx)
-    for ix = 1:lx
-        peps_d[ix] = fpeps[(ix-1)*ly+iy]
-        peps_u[ix] = permute(fpeps[(ix-1)*ly+ly-iy+1], (1, 2, 4), (3, 5))
-    end
-    mps_d[(iy-1)*lx+1:(iy-1)*lx+lx] = SRCcontract(mps_d[(iy-2)*lx+1:(iy-2)*lx+lx], peps_d, lx, bond_cut)
-    mps_u[(ly-iy)*lx+1:(ly-iy)*lx+lx] = SRCcontract(mps_u[(ly-iy+1)*lx+1:(ly-iy+1)*lx+lx], peps_u, lx, bond_cut)
-end
-for iy = 1:ly
-    for ix = 1:lx
-        mps_u[(ly-iy)*lx+ix] = permute(mps_u[(ly-iy)*lx+ix], (1, 2, 4), (3, 5))
-    end
-end
-# end
+ostringtest=[0.5 3 2 -3 -2;
+0.5 2 3 -3 -2;
+0.5 0 -3 1 0;
+0.5 3 0 -3 0;]
 
-ppeps = Vector{TensorMap}(undef, l)
-lt = Vector{TensorMap}(undef, lx)
-rt = Vector{TensorMap}(undef, lx)
-for ix = 1:lx
-    for iy = 1:ly
-        if iy == 1
-            PEPSlc!(lt, rt, mps_u[lx+1:lx+lx], 0, fpeps, iy, lx)
-            if ix == 1
-                pPEPSlc!(ppeps, 0, rt, mps_u[lx+1:lx+lx], 0, ix, iy, lx)
-            elseif ix == lx
-                pPEPSlc!(ppeps, lt, 0, mps_u[lx+1:lx+lx], 0, ix, iy, lx)
-            else
-                pPEPSlc!(ppeps, lt, rt, mps_u[lx+1:lx+lx], 0, ix, iy, lx)
-            end
-        elseif iy == ly
-            PEPSlc!(lt, rt, 0, mps_d[(ly-2)*lx+1:(ly-2)*lx+lx], fpeps, iy, lx)
-            if ix == 1
-                pPEPSlc!(ppeps, 0, rt, 0, mps_d[(ly-2)*lx+1:(ly-2)*lx+lx], ix, iy, lx)
-            elseif ix == lx
-                pPEPSlc!(ppeps, lt, 0, 0, mps_d[(ly-2)*lx+1:(ly-2)*lx+lx], ix, iy, lx)
-            else
-                pPEPSlc!(ppeps, lt, rt, 0, mps_d[(ly-2)*lx+1:(ly-2)*lx+lx], ix, iy, lx)
-            end
-        else
-            PEPSlc!(lt, rt, mps_u[(iy)*lx+1:(iy)*lx+lx], mps_d[(iy-2)*lx+1:(iy-2)*lx+lx], fpeps, iy, lx)
-            if ix == 1
-                pPEPSlc!(ppeps, 0, rt, mps_u[(iy)*lx+1:(iy)*lx+lx], mps_d[(iy-2)*lx+1:(iy-2)*lx+lx], ix, iy, lx)
-            elseif ix == lx
-                pPEPSlc!(ppeps, lt, 0, mps_u[(iy)*lx+1:(iy)*lx+lx], mps_d[(iy-2)*lx+1:(iy-2)*lx+lx], ix, iy, lx)
-            else
-                pPEPSlc!(ppeps, lt, rt, mps_u[(iy)*lx+1:(iy)*lx+lx], mps_d[(iy-2)*lx+1:(iy-2)*lx+lx], ix, iy, lx)
-            end
-        end
-    end
-end
-norm(lt[lx])
-norm(rt[1])
-for i = 1:l
-    println("Site $(i) fpepsnorm: ", norm(fpeps[i]))
-    println("Site $(i) ppepsnorm: ", norm(ppeps[i]))
-    # println("Site $(i) max: ", maximum(abs.(fpeps[i].data)))
-end
-
-
-
+olocal=observable_config(ostringtest, m, cm[3], configtest[3, :])
--- VMC_MPS/mpstools.jl
+++ VMC_MPS/mpstools.jl
@@ -1,76 +1,64 @@
-#For SRC compression of boundary MPS
-function SRCcontract(pepsi1, pepsi2, li, bond_cut)
-    # bond_cut = 10
-    # li = lx
-    # pepsi1 = Vector{TensorMap}(undef, li)
-    # pepsi2 = Vector{TensorMap}(undef, li)
-    # for ix = 1:li
-    #     pepsi1[ix] = peps[(ix-1)*ly+1]
-    #     pepsi2[ix] = peps[(ix-1)*ly+2]
-    # end
-    svddim = bond_cut
-    bond_cut = svddim * 2
-    Ωlist = Vector{TensorMap}(undef, li - 1)
-    for il = 1:li-1
-        Ωlist[il] = TensorMap(randn, ComplexF64, domain(pepsi2[il], 1) ← Rep[U₁](blocksectors(domain(pepsi2[il], 1))[1] => bond_cut))
+#For initial mps
+function initial_mps(l, n, phySpace, bond_dim)
+    bond_list = []
+    push!(bond_list, Vect[FermionParity⊠U1Irrep]((0, 0) => 1))
+    for i = 1:(l-1)
+        push!(bond_list, fuse(bond_list[end], phySpace))
     end
-    Blist = Vector{TensorMap}(undef, li)
-    for il = 1:li
-        piso = isometry(fuse(codomain(pepsi2[il], 1) ⊗ codomain(pepsi1[il], 1)) ← codomain(pepsi2[il], 1) ⊗ codomain(pepsi1[il], 1))
-        @tensor Blist[il][p l2 l1 d1; u2 r2 r1] := pepsi2[il][p2 l2 o1; u2 r2] * pepsi1[il][p1 l1 d1; o1 r1] * piso[p; p2 p1]
-    end
-    Clist = Vector{TensorMap}(undef, li - 1)
-    liso = isometry(fuse(codomain(Blist[1], 2) ⊗ codomain(Blist[1], 3)) ← codomain(Blist[1], 2) ⊗ codomain(Blist[1], 3))
-    @tensor Clist[1][p l d; u r2 r1] := Ωlist[1][o1; u] * Blist[1][p l2 l1 d; o1 r2 r1] * liso[l; l2 l1]
-    for il = 2:li-1
-        data = zeros(ComplexF64, bond_cut, bond_cut, bond_cut)
-        for i = 1:bond_cut
-            data[i, i, i] = 1.0
-        end
-        dbash = TensorMap(data, domain(Clist[il-1], 1) ⊗ domain(Ωlist[il], 1) ← Rep[U₁](blocksectors(fuse(domain(Clist[il-1], 1), domain(Ωlist[il], 1)))[1] => bond_cut))
-        @tensor Cbash[p l d2 d1; u r2 r1] := Clist[il-1][p l d1; o1 r2 r1] * Ωlist[il][d2; o2] * dbash[o1 o2; u]
-        piso = isometry(fuse(codomain(Blist[il], 1) ⊗ codomain(Cbash, 1)) ← codomain(Blist[il], 1) ⊗ codomain(Cbash, 1))
-        diso = isometry(fuse(codomain(Blist[il], 4) ⊗ codomain(Cbash, 4)) ← codomain(Blist[il], 4) ⊗ codomain(Cbash, 4))
-        @tensor Clist[il][p l d; u r2 r1] := Cbash[p1 l o3 d1; u o2 o1] * Blist[il][p2 o2 o1 d2; o3 r2 r1] * piso[p; p2 p1] * diso[d; d2 d1]
-    end
-    #   Clist[p l d; u r2 r1] diagram:
-    #     ↓
-    #  ←  o(i) ←
-    #   ↙ ↓    ←
-    #  p
-    #   Blist[p l2 l1 d; u r2 r1] diagram:
-    #      ↓
-    #  ←   o(i) ←
-    #  ← ↙ ↓    ←
-    #  p
+    push!(bond_list, Vect[FermionParity⊠U1Irrep]((0, n) => 1))
 
-    #QB decomposition and truncation
-    mps = Vector{TensorMap}(undef, li)
-    @tensor Ynow[p1 l d1 u1; p2 d2 u2 r2 r1] := Clist[li-1][p1 l d1; u1 o2 o1] * Blist[li][p2 o2 o1 d2; u2 r2 r1]
-    l, q = rightorth(Ynow, (1, 2, 3, 4), (5, 6, 7, 8, 9))
-    riso = isometry(domain(q, 4) ⊗ domain(q, 5) ← fuse(domain(q, 4) ⊗ domain(q, 5)))
-    @tensor mps[li][p l d; u r] := q[l; p d u r2 r1] * riso[r2 r1; r]
-    qp = permute(q, (2, 1, 3), (4, 5, 6))'
-    @tensor Rnow[l2 l1; r] := qp[o0 r2 r1; p r d] * Blist[li][p l2 l1 d; o0 r2 r1]
-    for il = (li-1):-1:2
-        @tensor Rnow[p l2 l1 d; u1 r] := Rnow[o2 o1; r] * Blist[il][p l2 l1 d; u1 o2 o1]
-        @tensor Ynow[p1 l d1 u; p2 d2 u1 r] := Rnow[p2 o2 o1 d2; u1 r] * Clist[il-1][p1 l d1; u o2 o1]
-        l, q = rightorth(Ynow, (1, 2, 3, 4), (5, 6, 7, 8))
-        mps[il] = permute(q, (2, 1, 3), (4, 5))
-        qp = mps[il]'
-        @tensor Rnow[l2 l1; r] := qp[o1 o2; p r d] * Rnow[p l2 l1 d; o1 o2]
-    end
-    liso = isometry(fuse(codomain(Blist[1], 2) ⊗ codomain(Blist[1], 3)) ← codomain(Blist[1], 2) ⊗ codomain(Blist[1], 3))
-    @tensor mps[1][p l d; u r] := Rnow[o2 o1; r] * Blist[1][p l2 l1 d; u o2 o1] * liso[l; l2 l1]
+    m = FiniteMPS([randn(Float64, bond_list[i] ⊗ phySpace ← bond_list[i+1]) for i in 1:l])
+    @show [dim(space(m[i], 3)) for i in 1:l]
 
-    #SVD compression
-    for il = 1:li-1
-        @tensor cmps[p1 l1 d1 u1; p2 d2 u2 r2] := mps[il][p1 l1 d1; u1 o0] * mps[il+1][p2 o0 d2; u2 r2]
-        mps[il], S, V, ϵ = tsvd(cmps, (1, 2, 3, 4), (5, 6, 7, 8); trunc=truncdim(svddim), alg=TensorKit.SVD())
-        println("Truncate bond between $(il) and $(il+1): Error=$(ϵ) D=$(dim(domain(mps[il])))")
-        mps[il] = permute(mps[il], (1, 2, 3), (4, 5))
-        mps[il+1] = permute(S * V, (2, 1, 3), (4, 5))
-    end
-    return mps
+    m = changebonds(m, SvdCut(trscheme=truncdim(bond_dim)))
+    @show [dim(space(m[i], 3)) for i in 1:l]
+
+    return m
 end
 
+#For the configuration coefficient of mps
+function get_config(m, config)
+    bond_list = []
+    push!(bond_list, Vect[FermionParity⊠U1Irrep]((0, 0) => 1))
+    for i = 1:l
+        push!(bond_list, fuse(bond_list[end], Vect[FermionParity⊠U1Irrep]((config[i], config[i]) => 1)))
+    end
+    mp = FiniteMPS([ones(Float64, bond_list[i] ⊗ phySpace ← bond_list[i+1]) for i in 1:l])
+    cm = MPSKit.dot(mp, m)
+
+    return cm
+end
+
+#For computing observables
+function observable_config(ostring, m, cm, config)
+    oterms=size(ostring, 1)
+    olength=size(ostring, 2)
+    oc=ostring[:, 1]
+
+    nconfig=[]
+    osum=0
+    for it=1:oterms
+        nconfig=copy(config)
+        for il=0:(olength-2)
+            oind=Int(ostring[it, end-il])
+            if oind==0
+                continue
+            elseif oind>0&&nconfig[oind]==0
+                oc[it]=oc[it]*(-1)^sum(nconfig[1:(oind-1)])
+                nconfig[oind]=1
+            elseif oind<0&&nconfig[-oind]==1
+                oc[it]=oc[it]*(-1)^sum(nconfig[1:(-oind-1)])
+                nconfig[-oind]=0
+            else
+                oc[it]=0
+                break
+            end
+        end
+        if oc[it]!=0
+            osum+=oc[it]*get_config(m, nconfig)
+        end
+    end
+    olocal=osum/cm
+
+    return olocal
+end
